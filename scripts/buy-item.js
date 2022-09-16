ac5c74d9a02c5f0735d38440087423d8b9108e71
Bartek <s210869@sggw.edu.pl>
2022-09-16 14:35:24 +0200

    added buy item script

--- scripts/buy-item.js
+++ scripts/buy-item.js
@@ -2,18 +2,21 @@ const { ethers, network } = require("hardhat");
 const { moveBlocks } = require("../utils/move-blocks");
 
 const TOKEN_ID = 0;
-async function cancel() {
+
+async function buyItem() {
     const nftMarketplace = await ethers.getContract("NftMarketplace");
     const basicNft = await ethers.getContract("BasicNft");
-    const tx = await nftMarketplace.cancelListing(basicNft.address, TOKEN_ID);
+    const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID);
+    const price = listing.price.toString();
+    const tx = await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, { value: price });
     await tx.wait(1);
-    console.log("NFT Canceled");
-    if ((network.config.chainId = 31337)) {
+    console.log("Bought NFT!");
+    if ((network.config.chainId = "31337")) {
         await moveBlocks(2, (sleepAmount = 1000));
     }
 }
 
-cancel()
+buyItem()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
