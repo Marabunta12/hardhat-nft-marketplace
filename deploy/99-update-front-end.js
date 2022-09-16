e264e5099eb18740a9d32ee8b1600dbc101f8f5b
Bartek <s210869@sggw.edu.pl>
2022-09-16 16:28:03 +0200

    update
    added updating abi

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -2,13 +2,29 @@ const { ethers, network } = require("hardhat");
 const fs = require("fs");
 
 const frontEndContractsFile = "../nextjs-nft-marketplace-moralis/constants/networkMapping.json";
+const frontEndAbiLocation = "../nextjs-nft-marketplace-moralis/constants/";
 
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Updating front end...");
         await updateContractAddresses();
+        await updateAbi();
     }
 };
+
+async function updateAbi() {
+    const nftMarketplace = await ethers.getContract("NftMarketplace");
+    fs.writeFileSync(
+        `${frontEndAbiLocation}NftMarketplace.json`,
+        nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
+    );
+
+    const basicNft = await ethers.getContract("BasicNft");
+    fs.writeFileSync(
+        `${frontEndAbiLocation}BasicNft.json`,
+        basicNft.interface.format(ethers.utils.FormatTypes.json)
+    );
+}
 
 async function updateContractAddresses() {
     const nftMarketplace = await ethers.getContract("NftMarketplace");
